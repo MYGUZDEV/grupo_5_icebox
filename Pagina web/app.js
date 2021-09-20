297f55ad7e494e874e1d2d7b607b3abea8ec70c9
LowOnCYAN23 <trikerdg@gmail.com>
2021-09-20 10:16:53 -0500

    Se actualizaron archivos dentro de Pagina web

--- Pagina web/app.js
+++ Pagina web/app.js
@@ -1,19 +1,35 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 
-const path = require ('path');
+const path = require('path');
 
-const publicPath = path.resolve(__dirname , './public');
-app.use (express.static(publicPath));
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
 
-app.listen (5000, () => {
-    console.log ('Servidor 5000 corriendo correctamente');
+app.listen(5000, () => {
+  console.log('Servidor 5000 corriendo correctamente');
 });
 
-app.get ('/' , (req , res) => {
-    res.sendFile (path.resolve(__dirname , './views/index.html' ));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/productDetail', (req, res) => {
+  res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+app.get('/productCart', (req, res) => {
+  res.sendFile(__dirname + '/views/productCart.html');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
